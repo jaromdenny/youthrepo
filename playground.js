03f074980f3053c372d502d7c51b86d0a279f494
jaromdenny <jaromdenny@gmail.com>
2020-08-05 13:20:24 +1000

    Create playground.js

---
+++ playground.js
@@ -0,0 +1,18 @@
+//https://codesandbox.io/
+//https://repl.it/
+
+var name = 'john';
+var number = 0;
+console.log(name);
+setTimeout(() => {console.log(name)}, 1000);
+
+name = 'My name is '+name;
+
+for (i = 0; i < 20; i++) {
+    console.log(i)
+}
+
+number = 3184 * 482932;
+
+
+console.log(number);
