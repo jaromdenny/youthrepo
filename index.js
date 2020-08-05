960840927d2516cb44ec112f93fa9fd09ae81152
jaromdenny <jaromdenny@gmail.com>
2020-08-05 13:23:30 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+//https://codesandbox.io/s/lingering-tree-rm0p8?file=/src/styles.css
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
